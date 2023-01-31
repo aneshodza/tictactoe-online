55108ce785ad1dfaf8b1737cf01091b910fd6a51
Anes Hodza <anes.hodza@renuo.ch>
2023-01-31 13:17:11 +0100

    Implement socketio

--- server/index.js
+++ server/index.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
 });
